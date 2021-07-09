d70284f03d6aa82e2447eadcf96123088d803892
Arihant <arihant.jain67@gmail.com>
2021-07-10 01:37:56 +0530

    init commit

---
+++ src/components/comments/Comments.js
@@ -0,0 +1,61 @@
+import { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import CommentList from "./CommentsList";
+
+const Comments = () => {
+  const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+  const { quoteId } = params;
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === "completed" && loadedComments?.length > 0) {
+    comments = <CommentList comments={loadedComments} />;
+  }
+
+  const startAddCommentHandler = () => {
+    setIsAddingComment(true);
+  };
+
+  return (
+    <section className={classes.comments}>
+      <h2>User Comments</h2>
+      {!isAddingComment && (
+        <button className="btn" onClick={startAddCommentHandler}>
+          Add a Comment
+        </button>
+      )}
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={quoteId}
+          onAddedComment={addedCommentHandler}
+        />
+      )}
+      <p>{comments}</p>
+    </section>
+  );
+};
+
+export default Comments;
